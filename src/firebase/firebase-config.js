e55dc421d8f02eac91830a5411d74e7f722b375f
Eleazar <eleazarcaz@outlook.com>
2020-10-05 18:58:10 -0500

    Recuperando archivos

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,39 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+/* const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+}; */
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAe1q1iA1jLKFJg9MlEzdHNFpDotMwXqhE',
+  authDomain: 'login-app-44b5d.firebaseapp.com',
+  databaseURL: 'https://login-app-44b5d.firebaseio.com',
+  projectId: 'login-app-44b5d',
+  storageBucket: 'login-app-44b5d.appspot.com',
+  messagingSenderId: '67726808912',
+  appId: '1:67726808912:web:dc7ac87f61524cd491fe55',
+  measurementId: 'G-41E91EY4TT',
+};
+
+// if( process.env.NODE_ENV === 'test' ) {
+//     // testing
+//     firebase.initializeApp(firebaseConfigTesting);
+// } else {
+// dev/prod
+// firebase.initializeApp(firebaseConfig);
+// }
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { db, googleAuthProvider, firebase };
